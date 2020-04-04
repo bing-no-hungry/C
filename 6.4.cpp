8874f787fd1feca2a0f81f57a73f64feacbf604f
bing-no-hungry <60037224+bing-no-hungry@users.noreply.github.com>
2020-04-04 18:05:54 +0800

    Add files via upload

---
+++ 6.4.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+main()
+{
+	int T,b,N,d,si,f=0,h=0,g;
+	scanf("%d",&T);
+	for(b=1;b<=T;b++)
+	{
+		scanf("%d",&N);
+		for(d=1;d<=N;d++)
+		{
+			scanf("%d",&si);
+			f=f+si;
+			if(f>0)
+			{
+				if(h<f)
+				h=f;
+			}
+            else
+            {
+            	if(d==1)
+            	g=si;
+            	else
+            	{
+            		if(si>g)
+            		g=si;
+				}
+				f=0;
+			}
+		}
+		if(f==0)
+		printf("%d",g);
+		else
+		printf("%d\n",h);
+		f=0;
+		h=0;
+	}
+	return 0;
+}
